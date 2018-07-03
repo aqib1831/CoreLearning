efb3a73aa6d81b679cf1d058e29952faf82c0512
maqib <muhammadaqib1831@outlook.com>
2018-07-03 10:06:02 +0500

    First chapter  pro asp.net core by james adam

---
+++ Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestApplicationCore.Models;
+
+namespace TestApplicationCore.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Moring" : "Good Afternoon";
+            return View();
+        }
+        [Route("Create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult Create(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
